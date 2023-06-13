162e5ea9b439aaf22348268baddf15dcd0070ae4
Samara2006 <samarahespanhol@gmail.com>
2023-06-13 15:11:25 -0400

    aa

---
+++ ProjetoTayUmDoce/ProjetoTayUmDoce/ProjetoTayUmDoce-main/README.md
@@ -0,0 +1,10 @@
+## ProjetoTayUmDoce
+
+## Integrantes
+
+### Amanda Rangel
+### Anderson Felipe
+### Felipe Torejiani 
+### Kaick Damas 
+### Samara Hespanhol
+### Tayná Carvalho
---
+++ ProjetoTayUmDoce/ProjetoTayUmDoce/ProjetoTayUmDoce-main/TayUmDoceProjeto original/TayUmDoceProjeto/Telas/CadastrarVenda.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TayUmDoceProjeto.Telas;
+
+namespace TayUmDoceProjeto.Telas
+{
+    /// <summary>
+    /// Lógica interna para CadastrarVenda.xaml
+    /// </summary>
+    public partial class CadastrarVenda : Window
+    {
+        public CadastrarVenda()
+        {
+            InitializeComponent();
+        }
+
+        private void btCliente_Click(object sender, RoutedEventArgs e)
+        {
+            CadastrarCliente form = new CadastrarCliente();
+            form.ShowDialog();
+        }
+
+        private void btEntrega_Click(object sender, RoutedEventArgs e)
+        {
+            CadastrarEntrega form = new CadastrarEntrega();
+            form.ShowDialog();
+        }
+
+        private void btDoce_Click(object sender, RoutedEventArgs e)
+        {
+            CadastrarDoce form = new CadastrarDoce();
+            form.ShowDialog();
+        }
+
+        private void btFuncionario_Click(object sender, RoutedEventArgs e)
+        {
+            CadastrarFuncionario form = new CadastrarFuncionario();
+            form.ShowDialog();
+        }
+
+        private void btFornecedor_Click(object sender, RoutedEventArgs e)
+        {
+            CadastrarFornecedor form = new CadastrarFornecedor();
+            form.ShowDialog();
+        }
+    }
+}
